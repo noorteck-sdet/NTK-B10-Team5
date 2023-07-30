5cb2bbb858abe58668483d0ea3ca6887c8fecff4
annelisses <140770721+annelisses@users.noreply.github.com>
2023-07-30 09:40:19 -0700

    Update DemoDay3.java
    Removed comments and updated code to include original main method, add(), add2() and included new divide().

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -1,11 +1,16 @@
 package demo;
 
 public class DemoDay3 {
-
-	//importance of following stash process && communicating... :)
+	public static void main(String[] args) {
+		System.out.println("Demo..");
+	}
+	public static void add( int a, int b) {
+		System.out.println(a+b);
+	}
 	public static add2( int a , int b , int c) {
 		System.out.println(a+b+c);
-
-			//Trap Note: never push conflict code...
+	}
+	public static void divide(int a, int b){
+		System.out.println(a/b);
 	}
 }
