c6d020707e88a530190c3a634f306a0b6335a7f4
Jefreey A <Jefreey A@DESKTOP-DN6I8K8>
2023-07-30 12:22:19 -0400

    initial

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -5,4 +5,8 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		System.out.println("Demo..");
 	}
+	
+	public static void add( int a, int b) {
+		System.out.println(a+b);
+	}
 }
