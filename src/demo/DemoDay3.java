4b1083d4ff1c2631c4e43c8109c1ec96daf77a1b
noorteck-sdet <119881887+noorteck-sdet@users.noreply.github.com>
2023-07-30 12:31:48 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -5,5 +5,7 @@ public class DemoDay3 {
 	//importance of following stash process && communicating... :)
 	public static add2( int a , int b , int c) {
 		System.out.println(a+b+c);
+
+			//Trap Note: never push conflict code...
 	}
 }
