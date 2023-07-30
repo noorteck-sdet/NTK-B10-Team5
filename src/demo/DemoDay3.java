79f50a64db2cfc95d6ddecfe516830a0dc7820fb
AnnelisseM <AnnelisseM@CIA>
2023-07-30 09:44:05 -0700

    Updated add2()

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -4,13 +4,16 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		System.out.println("Demo..");
 	}
-	public static void add( int a, int b) {
-		System.out.println(a+b);
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
-	public static add2( int a , int b , int c) {
-		System.out.println(a+b+c);
+
+	public static void add2(int a, int b, int c) {
+		System.out.println(a + b + c);
 	}
-	public static void divide(int a, int b){
-		System.out.println(a/b);
+
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
 	}
 }
