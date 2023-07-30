08a3171e8bea699fd6606c5e554398634246bbd7
saifA <saifA@Saber.myfiosgateway.com>
2023-07-30 12:31:37 -0400

    created new add2

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -3,4 +3,7 @@ package demo;
 public class DemoDay3 {
 
 	//importance of following stash process && communicating... :)
+	public static add2( int a , int b , int c) {
+		System.out.println(a+b+c);
+	}
 }
